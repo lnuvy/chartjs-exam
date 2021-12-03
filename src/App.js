a688776da660f84bb83e47228bb0d1964c852335
lnuvy <hangeulgod@naver.com>
2021-12-03 13:13:46 +0900

    테스트

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import VerticalBarChart from './VerticalBarChart';
+import HorizontalBarChart from './HorizontalBarChart'
+import LineChart from './LineChart'
+
+function App() {
+
+  return (
+    <div>
+      <VerticalBarChart />
+      <HorizontalBarChart />
+      <LineChart />
+      <p>하하</p>
+    </div>
+  );
+}
+
+export default App;
