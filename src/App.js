d8ad5e02ff8718263f76bf8da4ca2c092b1396cf
lnuvy <hangeulgod@naver.com>
2021-12-03 14:14:34 +0900

    data 전달구조 리팩토링

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import LineChart from './LineChart'
 import Layout from './Layout';
 
 function App() {
+  const data = [50, 40, 30, 35, 40]
+  const labels = [2017, 2018, 2019, 2020, 2021];
 
   return (
     <Layout>
-      <VerticalBarChart />
-      <HorizontalBarChart />
-      <LineChart />
+      <VerticalBarChart data={data} labels={labels} />
+      <HorizontalBarChart data={data} labels={labels} />
+      <LineChart data={data} labels={labels} />
     </Layout>
   );
 }
---
+++ src/VerticalBarChart.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from 'react';
+// import { registerables, Chart} from 'chart.js'
+// Chart.register(...registerables);
+// // Chart.register(BarController, CategoryScale, LinearScale)
+
+import Chart from 'chart.js/auto';
+
+function VerticalBarChart(props) {
+  const {data, labels} = props
+
+  const canvasDom = useRef(null);
+
+  // const RandomNumber = (arr) => {
+  //   let tmp = []
+  //   for(var i=0; i<arr.length; i++) {
+  //     let r = Math.random() * 5;
+  //     tmp[i] = arr[r]
+  //   }
+  //   return tmp;
+  // }
+
+  useEffect(() => {
+
+    const ctx = canvasDom.current.getContext('2d');
+    const verticalBarChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "월별 버스 이용량 통계",
+            data: data,
+            backgroundColor: 'rgba(255, 0, 0, 0.3)'
+          },
+          {
+            label: "월별 버스 이용량 통계",
+            data: data,
+            backgroundColor: 'rgba(0, 255, 0, 0.3)'
+          },
+          {
+            label: "월별 버스 이용량 통계",
+            data: data,
+            backgroundColor: 'rgba(0, 0, 255, 0.3)'
+          },
+        ],
+      },
+    });
+    return () => {
+      verticalBarChart.destroy();
+    }
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={canvasDom} />
+    </div>
+  );
+}
+
+export default VerticalBarChart;
